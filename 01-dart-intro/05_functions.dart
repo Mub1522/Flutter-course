6533afb63f579dc672992446a34aec88c5f2287e
mub <mubpersonal5@gmail.com>
2025-02-23 02:48:45 -0500

    Update: Required and name parameters

--- 01-dart-intro/05_functions.dart
+++ 01-dart-intro/05_functions.dart
@@ -2,6 +2,8 @@ void main() {
   print(greetEveryone());
   print(sumTwoNumbers(1, 3));
   print(sumTwoNumbersOptional(2));
+  print(greetPerson(name: 'Mub'));
+  print(greetPerson(name: 'Mub', message: 'Hi, '));
 }
 
 String greetEveryone() {
@@ -12,5 +14,10 @@ int sumTwoNumbers(int a, int b) => a + b;
 
 int sumTwoNumbersOptional(int a, [int b = 0]) {
   //b ??= 0;
+
   return a + b;
+}
+
+String greetPerson({required String name, String message = ''}){
+  return message + name;
 }
