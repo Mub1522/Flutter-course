2a4be4626143118671783d4052cc4e4fed4e6208
mub <mubpersonal5@gmail.com>
2025-02-27 21:06:57 -0500

    Update: Named constructors

--- 01-dart-intro/06_classes.dart
+++ 01-dart-intro/06_classes.dart
@@ -1,15 +1,37 @@
 void main(){
-  final Person person = Person('John', 30);
+  final Person person = Person(
+    name: 'Jhon',
+    age: 30
+  );
   print(person);
   print(person.name);
   print(person.age);
+
+  final Map<String, dynamic> personMap = {
+    'name': 'Jane',
+    'age': 25
+  };
+
+  final Person person2 = Person.fromJson(personMap);
+  print(person2);
+  print(person2.name);
+  print(person2.age);
 }
 
 class Person {
   String name;
   int age;
 
-  Person(this.name, this.age);
+  /* Person(this.name, this.age); */
+
+  Person({
+    required this.name,
+    required this.age
+  });
+
+  Person.fromJson(Map<String, dynamic> json)
+    : name = json['name'],
+      age = json['age'];
 
   @override
   String toString() {
